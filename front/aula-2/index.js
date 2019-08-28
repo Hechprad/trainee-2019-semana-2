19f7588e5e9b2c423df7732a326a86e9de0edba2
Jorge Hecherat <hecherat@gmail.com>
2019-08-28 11:34:30 -0300

    att

---
+++ front/aula-2/index.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+
+console.log('abc');
+
+var linguagens = 'javascript'
+
+console.log(linguagens);
+
+var linguagens = 4444;
+console.log(linguagens);
+
+leu alo = "ashiuahsiuhaiuhsa";
+alo = 2332323232;
+
+const constante = "aqui é uma constante";
+
+contante = 2;
+
+const pessoa = {
+  nome: 'joaquim'
+}
+pessoa.altura = 1.9;
+console.log(pessoa);
